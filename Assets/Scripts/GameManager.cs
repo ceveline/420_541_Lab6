d645975f3f58d401e72187a3cb1948aa4b0e9d68
Ceveline <74314874+ceveline@users.noreply.github.com>
2024-09-30 15:30:00 -0400

    finished

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Add this namespace
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int score = 0;
+    public int targetScore = 4; // Score to reach before changing scenes
+
+    void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Optional
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        Debug.Log("Score: " + score);
+
+        //if (score >= targetScore)
+        //{
+        //    LoadNewScene();
+        //}
+    }
+
+    public void LoadNextScene()
+    {
+        //SceneManager.LoadScene("VictoryScene"); // Use your new scene's name
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
